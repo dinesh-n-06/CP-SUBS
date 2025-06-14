b8b9561bb42ca41703135f9a63795dd2c5a74b57
dineshhh-06 <dineshnaragani4295@gmail.com>
2025-06-14 21:26:42 +0530

    CF probs\

---
+++ CodeForces/Problems/A_Coins.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int t;
+    cin >> t;
+    while (t-- != 0) {
+        int n,k;
+        cin >> n>>k;
+        cout<<((n%2==0 || (n-k)%2==0)?"YES":"NO")<<endl;
+    }
+
+    return 0;
+}
