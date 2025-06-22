dfffd50da8a83069fc35a5813d4feab2585f63c4
dinesh-n-06 <dineshnaragani4295@gmail.com>
2025-06-22 23:16:50 +0530

    cf

--- A_Make_it_Beautiful.cpp
+++ A_Make_it_Beautiful.cpp
@@ -8,9 +8,21 @@ int main() {
     while (t-- != 0) {
         int n;
         cin >> n;
-        
-        
+        int arr[n];
+        for (int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        sort(arr,arr+n);
+        if (arr[0]==arr[n-1]) cout<<"NO"<<endl;
+        else{
+            swap(arr[1],arr[0]);
+            swap(arr[n-1],arr[0]);
+            cout<<"YES"<<endl;
+            for (int i:arr){
+                cout<<i<<" ";
+            }
+            cout<<endl;
+        }
     }
-
     return 0;
 }
