41c80181c4b280884875387f1e4167ebe713322c
dinesh-n-06 <dineshnaragani4295@gmail.com>
2025-06-25 21:46:39 +0530

    cf prob

---
+++ A_Two_Permutations.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int t;
+    cin >> t;
+    while (t-- != 0) {
+        int n,a,b;
+        cin >> n >> a >>b;
+        if (a+b+2<=n || (a==b&&b==n)) cout<< "Yes" << endl;
+        else cout << "No" << endl;   
+    }
+    return 0;
+}
