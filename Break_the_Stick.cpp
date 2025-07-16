695c94529ffb9b262d6ff62126215e8fee15863b
dinesh-n-06 <dineshnaragani4295@gmail.com>
2025-07-16 23:18:54 +0530

    cc

---
+++ Break_the_Stick.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int t;
+    cin >> t;
+    while (t-- != 0) {
+        int n,x;
+        cin >> n >> x;
+        cout<<((x%2==0 && n%2 == 1)?"NO":"YES")<<endl;
+        
+        
+    }
+
+    return 0;
+}
