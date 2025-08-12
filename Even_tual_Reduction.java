50db5483cc1402c4b396c6663f677f91d605703e
dinesh-n-06 <dineshnaragani4295@gmail.com>
2025-08-12 23:15:26 +0530

    c

---
+++ Even_tual_Reduction.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Even_tual_Reduction {
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t--!=0){
+		    int n = sc.nextInt();
+		    sc.nextLine();
+		    String s = sc.nextLine();
+		    HashMap<Character, Integer> map = new HashMap<>();
+		    // char[] arr = s.toCharArray();
+		    for(char i: s.toCharArray()){
+		        if(!map.containsKey(i))
+		            map.put(i,1);
+		        else
+		            map.put(i,map.get(i)+1);
+		    }
+		    boolean yes = true;
+		    for(int i: map.values()){
+		        if(i%2!=0)
+		            yes = false;
+		    }
+		    System.out.println((yes)?"YES":"NO");
+		    
+		}
+    }
+}
