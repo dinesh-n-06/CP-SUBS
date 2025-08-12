4f666a56ed111211a01cea17f724ec31f9288001
dinesh-n-06 <dineshnaragani4295@gmail.com>
2025-08-12 23:17:39 +0530

    cc

---
+++ Counting_Triangles.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Counting_Triangles {
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        while (t-- > 0) {
+            int n = s.nextInt();
+            
+        }
+    }
+}
